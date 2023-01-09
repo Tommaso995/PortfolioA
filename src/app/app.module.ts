a69fe2f62ea01f9cfd46437971bd7d97026744e9
Tommaso995 <tommaso.magliocca@gmail.com>
2023-01-09 13:50:15 +0100

    Aggiornamento
    Aggiunte icone nei contatti, sistemato il routerlink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { PagesModule } from "./pages/pages.module";
         BrowserModule,
         AppRoutingModule,
         CoreModule,
-        PagesModule
+        PagesModule,
+        RouterModule
     ]
 })
 export class AppModule { }
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,12 @@
 document.ready(function(){
 
-  $('#darkmode').on('click', function(){
+  $('#darkmode').on("click", function(){
+
 
     $('nav').addClass("navbarDark");
-  })
+  });
 
   $('#lightmode').click(function(){
-
     $('nav').removeClass("navbarDark")
-  })
+  });
 });
