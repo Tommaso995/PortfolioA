41873ed0fb992bc1d545390a27d1374bee872e25
Tommaso995 <tommaso.magliocca@gmail.com>
2023-01-21 12:23:05 +0100

    Inserimento progetto Generation dentro Progetti Web App
    Inserimento progetto Project Work Generation Italy. Corretto il data-bs-target presente nel carosello con [attr.data-bs-target]="'#' + progetti.idCarosello" . Sarebbe l' array contenente l' id dell'indice del carosello.

--- src/app/pages/progetti-web-app/progetti-web-app.component.ts
+++ src/app/pages/progetti-web-app/progetti-web-app.component.ts
@@ -11,7 +11,9 @@ export class ProgettiWebAppComponent {
     {
       titolo: 'Portfolio progetti',
     descrizione: `Questo Portfolio è stato realizzato utilizzando linguaggio HTML, CSS, Framework Bootstrap, Javascript, Jquery e il Framework Angular.`,
-  foto1:'../../../assets/img/carosello/1.png',
+    link:'https://portfolio-4cf9e.web.app/',
+    idCarosello:'carouselExampleIndicators',
+    foto1:'../../../assets/img/carosello/1.png',
   foto2: '../../../assets/img/carosello/2.png',
   foto3: '../../../assets/img/carosello/3.png',
   foto4: '../../../assets/img/carosello/4.png',
@@ -20,5 +22,19 @@ export class ProgettiWebAppComponent {
   foto7: '../../../assets/img/carosello/7.png',
   foto8: '../../../assets/img/carosello/8.png'
     },
+    {
+      titolo: 'Project Work - Generation Italy',
+    descrizione: `Questo progetto è stato realizzato al termine del corso erogato da Generation Italy per la figura Junior Java Developer, dove io mi sono occupato del lato Front End. Viene rappresentata una piattaforma dove le persone possono registrarsi e inserire le recensioni dei viaggi effettuati con relative foto (max 3 foto); l'admin dalla sua area può vedere se ci sono nuove recensioni in attesa di essere pubblicate e accettare di pubblicarle o meno visionando la recensione con la foto. Le persone che visiteranno il sito, sia persone registrate e non, dalla schermata Home possono visualizzare le recensioni e se vogliono filtrarle in base al luogo (Italia, Europa e Altro). Il tutto è stato progettato utilizzando il linguaggio: HTML, CSS, Framework Bootstrap, Javascript, Jquery, Framework Spring, Data Base MySQL.`,
+    link:'https://github.com/ProtoR0/Project-Viaggi_Generation',
+    idCarosello:'progettoGeneration',
+    foto1:'../../../assets/img/carosello/caroselloProgettoGeneration/ProjectGeneration1.png',
+  foto2: '../../../assets/img/carosello/caroselloProgettoGeneration/ProjectGeneration3.png',
+  foto3: '../../../assets/img/carosello/caroselloProgettoGeneration/ProjectGeneration4.png',
+  foto4: '../../../assets/img/carosello/caroselloProgettoGeneration/ProjectGeneration5.png',
+  foto5: '../../../assets/img/carosello/caroselloProgettoGeneration/ProjectGeneration6.png', /* Inserire le foto del progetto */
+  foto6: '../../../assets/img/carosello/caroselloProgettoGeneration/ProjectGeneration7.png',
+  foto7: '../../../assets/img/carosello/caroselloProgettoGeneration/ProjectGeneration8.png',
+  foto8: '../../../assets/img/carosello/caroselloProgettoGeneration/ProjectGeneration9.png'
+    }
   ]
 }
